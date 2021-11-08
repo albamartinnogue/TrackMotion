531504216c28d2c2ecd923659a70a62f4c752660
Daniel Baquero Zazo <dbaquerozao@gmail.com>
2021-11-08 20:12:36 +0100

    Grafica codo flexion

---
+++ Graficas/GraficaFlexionCodo.m
@@ -0,0 +1,20 @@
+%componente x de puntos originales
+xCF = 0:0.1:1;
+
+%componente y de puntos originales
+yCF = [67,70,89,108,115,120,115,107,83,72,67];
+
+yREF1 = [0,0,0,0,0,0,0,0,0,0,0];
+yREF2 = [150,150,150,150,150,150,150,150,150,150,150];
+
+%comonente x de evaluacion
+xRCF = 0:0.001:1;
+
+%Para quitar los picos
+pp= makima(xCF,yCF);
+
+m= ppval(pp, xRCF);
+
+plot(xRCF,m,'g');hold on, grid on,title ('Flexión Codo')
+plot(xCF,yREF1);hold on
+plot(xCF,yREF2);
