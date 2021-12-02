144b5336f6455e93de4980f4c0d746f499800bf1
Daniel Baquero Zazo <dbaquerozao@gmail.com>
2021-12-02 12:25:11 +0100

    Aleatoriedad incompleta, pero por buen camino

---
+++ Graficas/AleatoriedadAbdHombro.m
@@ -0,0 +1,40 @@
+function [xmak,ymak,xnmak,ynmak] = AleatoriedadAbdHombro ( noise)
+%eje x de la señal
+%xvar = [x0, x1] que indica el valor maximo y minimo del final del eje x
+%y = señal
+%yvar = [y0, y1] factor de multiplicacion maximo y minimo
+%points= puntos donde evaluar makima
+%noise = si aplicar rido o no
+
+
+%nueva x
+xHA = 0:0.1:1;
+xnHA = xHA+ 0,01*rand(1,11); 
+
+T=(xnHA(end)-xnHA(1))/length(xnHA);
+rxT = T.*rand(1, length(xnHA))-(T/2);
+rxT(1) = 0;
+xnHA=xnHA+rxT;
+
+
+%nueva y
+yHA = [10,14,19,26,31,33,30,26,21,16,10];
+ynHA = yHA + 5*rand(1,11);
+ynHA(1) = yHA(1);
+ynHA(end) = yHA(end);
+
+%----------------------------
+xRHA = 0:0.001:1;
+
+ppHA= makima(xnHA,ynHA);
+
+mHA= ppval(ppHA, xRHA);
+
+plot(xRHA,mHA,'r');
+
+if noise == 1
+    mHA = mHA + 3*rand(1,1001);
+    
+end
+
+%plot(xnmak,ynmak,'r'); grid on,title ('Abducción Hombro Malo')
