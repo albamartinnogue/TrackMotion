be965e434ee7954c7efbb47c4fe252fffbb98e3a
Daniel Baquero Zazo <dbaquerozao@gmail.com>
2021-12-10 18:16:14 +0100

    Movimiento arreglado

--- Avatar/Movimiento.m
+++ Avatar/Movimiento.m
@@ -1,4 +1,4 @@
-% CreacionAvatar([0,10,-40],[25,0,-67],[0,5,-27]); 1º movimiento
+%CreacionAvatar([0,10,-40],[25,0,-67],[0,5,-27]); 1º movimiento
 % CreacionAvatar([0,14,-42],[28,0,-70],[0,6,-25]); 2º movimiento
 % CreacionAvatar([0,19,-45],[35,0,-89],[0,10,-23]); 3º movimiento
 % CreacionAvatar([0,26,-50],[42,0,-108],[0,12,-20]); 4º movimiento
@@ -7,11 +7,42 @@
 % CreacionAvatar([0,30,-56],[54,0,-115],[0,15,-22]); 7º movimiento
 % CreacionAvatar([0,26,-50],[25,0,-107],[0,12,-20]); 8º movimiento
 % CreacionAvatar([0,21,-45],[17,0,-83],[0,9,-23]); 9º movimiento
-% CreacionAvatar([0,16,-42],[10,0,-72],[0,6,-25]); 10º movimiento
-% CreacionAvatar([0,10,-40],[25,0,-67],[0,5,-27]); 11º movimiento
+%CreacionAvatar([0,16,-42],[10,0,-72],[0,6,-25]); 10º movimiento
+%CreacionAvatar([0,10,-40],[25,0,-67],[0,5,-27]); 11º movimiento
 
-CreacionAvatar([0,10,-40],[25,0,-67],[0,5,-27]);
-pause(1.5); 
-close all
+
+CreacionAvatar([0,10,-40],[25,0,-67],[0,5,-27]); 
+pause(1); 
+closereq();
 CreacionAvatar([0,14,-42],[28,0,-70],[0,6,-25]);
+pause(1);
+closereq();
+ CreacionAvatar([0,19,-45],[35,0,-89],[0,10,-23]);
+ pause(1);
+ closereq();
+ CreacionAvatar([0,26,-50],[42,0,-108],[0,12,-20]);
+ pause(1);
+ closereq();
+ CreacionAvatar([0,31,-56],[52,0,-115],[0,15,-21]); 
+ pause(1);
+ closereq();
+ CreacionAvatar([0,33,-60],[51,0,-120],[0,16,-20]); 
+ pause(1);
+ closereq();
+ CreacionAvatar([0,30,-56],[54,0,-115],[0,15,-22]); 
+ pause(1);
+ closereq();
+ CreacionAvatar([0,26,-50],[25,0,-107],[0,12,-20]); 
+pause(1);
+closereq();
+CreacionAvatar([0,21,-45],[17,0,-83],[0,9,-23]); 
+ pause(1);
+ closereq();
+CreacionAvatar([0,16,-42],[10,0,-72],[0,6,-25]);
+pause(1);
+closereq();
+CreacionAvatar([0,10,-40],[25,0,-67],[0,5,-27]); 
+pause(1);
 
+
+%}
