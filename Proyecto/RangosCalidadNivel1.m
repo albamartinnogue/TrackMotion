192948211e850c587714c0322e7cd54c417958c9
albamartinnogue <73606429+albamartinnogue@users.noreply.github.com>
2022-01-22 13:02:50 +0100

    Rangos

---
+++ Proyecto/RangosCalidadNivel1.m
@@ -0,0 +1,26 @@
+function [mSUP, mINF] = RangosCalidadNivel1 (app, yIdeal,duration)
+
+%Grafica ideal
+%yIdeal = [10,14,19,26,31,33,30,26,21,16,10];
+
+x = 0:0.1:1;
+%Rangos
+rSUP = yIdeal + 3;
+rINF= yIdeal - 3;
+
+xR = 0:0.001:1;
+
+%makima rango SUP
+    ppSUP= makima(x*duration,rSUP);
+    mSUP= ppval(ppSUP, xR*duration);
+
+%makima rango INF
+     ppINF= makima(x*duration,rINF);
+     mINF= ppval(ppINF, xR*duration);
+    
+ 
+  %  plot(xR*duration,mSUP, '--r'), hold on, plot(xR*duration,mINF, '--r')
+
+end
+
+
