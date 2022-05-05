2b0b8fffcadae6051c292d6e79565a4dd1db2bbd
arang <arang@LAPTOP-8Q4VVS04>
2022-05-05 21:51:06 +0200

    tiempo real

--- Proyecto/RangosCalidad.m
+++ Proyecto/RangosCalidad.m
@@ -1,4 +1,4 @@
-function [mSUP, mINF] = RangosCalidad ( app, yIdeal, duration, rango)
+function [mSUP, mINF] = RangosCalidad ( app, yIdeal, ejex, rango)
 
 %Grafica ideal
 %y = [10,14,19,26,31,33,30,26,21,16,10];
@@ -11,12 +11,12 @@ rINF= yIdeal - rango;
 xR = 0:0.001:1;
 
 %makima rango SUP
-    ppSUP= makima(x*duration,rSUP);
-    mSUP= ppval(ppSUP, xR*duration);
+    ppSUP= makima(x*ejex,rSUP);
+    mSUP= ppval(ppSUP, xR*ejex);
 
 %makima rango INF
-     ppINF= makima(x*duration,rINF);
-     mINF= ppval(ppINF, xR*duration);
+     ppINF= makima(x*ejex,rINF);
+     mINF= ppval(ppINF, xR*ejex);
     
  
    % plot(xR*duration,mSUP, 'r'), hold on, plot(xR*duration,mINF, 'r')
--- Proyecto/conexion.m
+++ Proyecto/conexion.m
@@ -3,21 +3,28 @@ configureTerminator(puerto,"CR/LF",1);
 puerto.UserData = struct('Data',[],'Count',1);
 readline(puerto)
 write(puerto, 's','string')
-for i = 1:5
+for i = 1:100
     readline(puerto);
 end
 flush(puerto);
 puerto.UserData.Data=[];
-i = 0;
-while(i<9000)
+muestras = 3900;
+x1 = 1:5 :muestras;
+n = numel(x1);
+i = 1;
+while(i<muestras)
     readData(puerto);
+    x = puerto.UserData.Data(:,1);
+    plot(x1(1:i),x(1:i),'r');
+drawnow;
+y = puerto.UserData.Data(:,2);
+plot(x1(1:i),y(1:i),'g');
+drawnow;
+z = puerto.UserData.Data(:,3);
+plot(x1(1:i),z(1:i),'b');
+drawnow;
     i=i+1;
 end
-x = puerto.UserData.Data(:,1);
-y = puerto.UserData.Data(:,2);
-z = puerto.UserData.Data(:,3);
-%coloresplot(x)
-hold on
-plot(y,'r')
-hold on
-plot(z,'g')
+%Escribir en excel
+M = [x,y,z];
+xlswrite('PruebaExcel.xlsx',M)
