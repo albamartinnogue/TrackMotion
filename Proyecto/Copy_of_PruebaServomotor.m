3e5b81e7118cff49389bad486c911609af3dc1bb
arang <arang@LAPTOP-8Q4VVS04>
2022-05-31 14:33:31 +0200

    d

--- Proyecto/Copy_of_PruebaServomotor.m
+++ Proyecto/Copy_of_PruebaServomotor.m
@@ -1,21 +1,35 @@
-
-/
 clear all port;
 a = arduino('COM8');
-s=servo(a,'A0'); 
+s1=servo(a,'A0');
+s2 =servo(a,'A1');
 
-datos = xlsread('testdata.xlsx','Hoja2');
-datosFlexion = datos(:,1);
+%datos = xlsread('MovimientosIdeales.xlsx','Hoja1');
+% datosFlexionHombro = datos(:,1);
+% datosFlexionCodo = datos(:,3);
 
-datosFlexion = datosFlexion*1/180;
+datosFlexionCodo = 0;
+
+
+%datosFlexionHombro = datosFlexionHombro*1/180;
+%datosFlexionCodo = datosFlexionCodo*1/180;
+
+writePosition(s1,datosFlexionCodo)
+
 
 %datosFlexion = 0:10:length(datosFlexion);
 
-for i=1 : length(datosFlexion)
-    writePosition(s,datosFlexion(i,1))
-       i=i+1;
-       pause(1)
-end
+% for i=1 : length(datosFlexionHombro)
+%     writePosition(s1,datosFlexionHombro(i,1))
+%     % writePosition(s2,datosFlexionCodo(i,1))
+%        i=i+1;
+%        pause(1)
+% end
+
+% for i=1 : length(datosFlexionCodo)
+%     writePosition(s2,datosFlexionCodo(i,1))
+%        i=i+1;
+%        pause(1)
+% end
 
 
 
